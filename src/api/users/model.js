1b07ab1e154171fd069b447bd5813bd61ecc69c7
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-24 11:14:07 +0100

    feat: create User model

--- src/api/users/model.js
+++ src/api/users/model.js
@@ -2,8 +2,36 @@ const { DataTypes } = require("sequelize");
 const { context } = require("../../db/connection");
 
 /**
- * Defines schema for a record, within the Books table, in the database.
+ * Defines schema for a record, within the Users table, in the database.
  */
-const Book = context.define("Book", {});
+const User = context.define(
+	"User",
+	{
+		username: {
+			type: DataTypes.STRING,
+		},
+		email: {
+			type: DataTypes.STRING,
+		},
+		password: {
+			type: DataTypes.STRING,
+		},
+	},
+	{
+		// This shows a different way to determine unique fields.
+		// However, it can be better to define the properties of the
+		// fields within the field definition, above.
+		indexes: [
+			{
+				allowNull: false,
+				fields: ["username", "email", "password"],
+			},
+			{
+				unique: true,
+				fields: ["username", "email"],
+			},
+		],
+	},
+);
 
-module.exports = Book;
+module.exports = User;
