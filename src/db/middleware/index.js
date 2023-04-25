f25f6144462f63055963ce6b75438bdebdc1bb6f
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-25 02:58:46 +0100

    feat: add middleware to routes

--- src/db/middleware/index.js
+++ src/db/middleware/index.js
@@ -8,4 +8,14 @@ module.exports = {
 	 * Compares a cleartext password, with the hashed using bcrypt.
 	 */
 	comparePass: require("./comparePass"),
+
+	/**
+	 * Validates the strength of a cleartext password, using the OWASP Password Strength Guidelines.
+	 */
+	validatePass: require("./validatePass"),
+
+	/**
+	 * Validates an email address, using the HTML5 email validation rules.
+	 */
+	validateEmail: require("./validateEmail"),
 };
