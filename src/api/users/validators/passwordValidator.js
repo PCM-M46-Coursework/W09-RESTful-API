594eea896eb9ff59f6a644535414e5a6d28b940b
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-25 11:46:08 +0100

    feat: add password validation unit tests

--- src/api/users/validators/passwordValidator.js
+++ src/api/users/validators/passwordValidator.js
@@ -6,7 +6,7 @@ module.exports = password => {
 	//    Contain at least one uppercase letter ((?=.*[A-Z]))
 	//    Contain at least one digit ((?=.*\d))
 	//    Contain at least one special character ((?=.*[!@#$%^&*()_+]))
-	//    Be at least 10 characters long ({12,})
+	//    Be at least 10 characters long ({10,})
 	//    Only contain letters, digits, and the specified special characters ([A-Za-z\d!@#$%^&*()_+ ])
 	const passwordRegex =
 		/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+ ]{10,}$/;
