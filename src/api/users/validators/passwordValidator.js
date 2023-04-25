b407f45680f5be67e2b687e5d70102675f925b93
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-25 03:12:13 +0100

    fix: use OWASP guidelines for password validation

--- src/api/users/validators/passwordValidator.js
+++ src/api/users/validators/passwordValidator.js
@@ -1,14 +1,14 @@
-/**
- * Checks if the password field in the request body meets the following industry standard conventions:
- *
- *  • Contains at least one lowercase letter
- *  • Contains at least one uppercase letter
- *  • Contains at least one digit
- *  • Contains at least one special character (!@#$%^&*()_+)
- *  • Is at least 8 characters long
- */
 module.exports = password => {
+	// Ref: https://owasp.deteact.com/cheat/cheatsheets/Authentication_Cheat_Sheet.html
+	// OWASP Password Strength Guidelines require the password to:
+	//
+	//    Contain at least one lowercase letter ((?=.*[a-z]))
+	//    Contain at least one uppercase letter ((?=.*[A-Z]))
+	//    Contain at least one digit ((?=.*\d))
+	//    Contain at least one special character ((?=.*[!@#$%^&*()_+]))
+	//    Be at least 10 characters long ({12,})
+	//    Only contain letters, digits, and the specified special characters ([A-Za-z\d!@#$%^&*()_+ ])
 	const passwordRegex =
-		/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])(?=.*[^\da-zA-Z]).{8,}$/;
+		/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+ ]{10,}$/;
 	return passwordRegex.test(password);
 };
