075439cb3a41164ba02e32729aed2fe9ea9c2a3b
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-25 01:44:04 +0100

    refactor: rename middleware to match design specs

--- src/db/middleware/index.js
+++ src/db/middleware/index.js
@@ -2,10 +2,10 @@ module.exports = {
 	/**
 	 * Hash a cleartext password, using bcrypt.
 	 */
-	hashPassword: require("./hashPassword"),
+	hashPass: require("./hashPass"),
 
 	/**
 	 * Compares a cleartext password, with the hashed using bcrypt.
 	 */
-	compareHash: require("./compareHash"),
+	comparePass: require("./comparePass"),
 };
