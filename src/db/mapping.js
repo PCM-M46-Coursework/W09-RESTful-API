fd863ceea857f00dcd867314ab6e38992b2f8423
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-26 01:33:19 +0100

    feat: add relational mappings

--- src/db/mapping.js
+++ src/db/mapping.js
@@ -1,4 +1,5 @@
 const User = require("../api/users/model");
+const Task = require("../api/tasks/model");
 
 module.exports = {
 	/**
@@ -6,10 +7,31 @@ module.exports = {
 	 */
 	synchroniseTables: function () {
 		User.sync({ alter: true });
+		Task.sync({ alter: true });
 	},
 
 	/**
 	 * Create relational mappings between tables.
 	 */
-	mapRelationships: function () {},
+	mapRelationships: function () {
+		/**
+		 * One -> Many Optional Relationship: User may have zero or more Tasks.
+		 */
+		User.hasMany(Task, {
+			foreignKey: {
+				name: "user_id",
+				allowNull: false,
+			},
+		});
+
+		/**
+		 * One -> One Required Relationship: Task has a User.
+		 */
+		Task.belongsTo(User, {
+			foreignKey: {
+				name: "user_id",
+				allowNull: false,
+			},
+		});
+	},
 };
